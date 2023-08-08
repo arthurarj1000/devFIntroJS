0a768f20d5c16b7f71bdecaf84e9c25ca7bef95c
arthurarj1000 <arturoarjim1000@gmail.com>
2023-08-07 20:53:58 -0600

    Clase1 Intro a JS version1.0

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log("Hola, soy Arturo :)");
+
+const number1 = 2;
+const number2 = 10;
+
+const sum = number1 + number2;
+const multiplication = number1 * number2;
+
+console.log(sum);
+console.log(multiplication);
+
+console.log("La suma de "+number1 + " con " +number2 + " es igual a" + sum);
