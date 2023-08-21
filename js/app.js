619ae2e1db8461a6ac2eb3ab2e76b7bb79ca47f5
arthurarj1000 <arturoarjim1000@gmail.com>
2023-08-21 16:37:58 -0600

    Listo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// INTRO AL JAVA y sus funciones PRIMARIAS
+
 // console.log("Hola, soy Arturo :)");
 
 // const number1 = 2;
@@ -46,16 +48,73 @@
 // console.log("La multiplicación de: " +number1 + " con " +number2 + " es igual a " + multiplication);
 
 // Ejercicio2, calcular su edad y año de nacimiento
-const name= prompt("Dime tu nombre");
+// const name= prompt("Dime tu nombre");
 
 
-alert("Hola " + name + "!")
+// alert("Hola " + name + "!")
 
-const birth_year= prompt("En qué año naciste?");
-const actual_year= 2023;
+// const birth_year= prompt("En qué año naciste?");
+// const actual_year= 2023;
 
-// const age= actual_year - birth_year;
-const age= actual_year - Number(birth_year); 
+// // const age= actual_year - birth_year;
+// const age= actual_year - Number(birth_year); 
 
 
-alert("Tienes " + age + " años!");
+// alert("Tienes " + age + " años!");
+
+/* -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+// CONDICIONALES/ IFs
+
+// Ejemeplo 1
+// let age=20;
+
+// if( age >= 18) console.log("Eres mayor de edad");
+
+// console.log ("Fin");
+
+
+// Ejemplo 2
+// let age = 18;
+
+// if( age >= 18){
+//     console.log("Tienes " + age + " años");
+//     console.log("Eres mayor de edad");
+// }
+// console.log("Fin");
+
+
+// Ejemplo 3 con IF y ELSE
+// let age=10;
+// if(age >=18){
+//     console.log("Eres mayor de edad");
+// } else{
+//     console.log("Eres menor de edad");
+// }
+
+
+// // Ejemplo 4 SEMAFORO con ELSE IF
+// let light="rojo";
+
+// if(light === "rojo"){
+//     console.log("No avances");
+// }else if(light === "amarillo"){
+//     console.log("Baja velocidad");
+// }else if(light === "verde"){
+//     console.log("Avanza");
+// }
+
+// EJERCICIO números pares
+// const number = prompt("Pon un número");
+//  if( number%2 === 0)
+//  {
+//     console.log("El número " + number + " es par! :)");
+//     alert("El número " + number + " es par! :)")
+//  }else{
+//     console.log("El número " + number + " no es par :(");
+//     alert("El número " + number + " no es par :(")
+//  }
+
+
+// BACKTICKS
+
